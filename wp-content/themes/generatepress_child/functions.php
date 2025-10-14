f9b6579057756f8fc6f7f6362b3e03b7c9163bc9
upulsgamage <usgamage@gmail.com>
2025-10-14 14:58:48 +1100

    feat: Implement brand typography
    - Set Playfair Display for headings (Authority Font)
    - Set Inter for body and UI elements (Readability & Functional Font)

--- wp-content/themes/generatepress_child/functions.php
+++ wp-content/themes/generatepress_child/functions.php
@@ -23,3 +23,11 @@ function generatepress_child_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts' );
+
+/**
+ * Enqueue Google Fonts
+ */
+function generatepress_child_enqueue_google_fonts() {
+    wp_enqueue_style( 'generatepress-child-google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Playfair+Display:wght@700&display=swap', false );
+}
+add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_google_fonts' );
